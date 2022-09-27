c8fcf5a195429295b99c2c8523a830b829ac9b52
Nicholas Gan Shyan <nicholasganshyan@gmail.com>
2022-09-27 16:33:57 +0800

    Initial commit for duckiebot_interface package
    Package acts as the main launch package for the various interface drivers.

---
+++ duckiebot_interface/setup.py
@@ -0,0 +1,29 @@
+import os
+
+from setuptools import setup
+from glob import glob
+
+package_name = 'duckiebot_interface'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.xml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Mack',
+    maintainer_email='mack@duckietown.org',
+    description='The duckiebot_interface meta package',
+    license='GPLv3',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
